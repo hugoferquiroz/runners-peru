098c83b70fcb5039102a5b6b464a64aa612acce0
Hugo <hugofernandezquiroz@gmail.com>
2021-11-23 12:43:32 -0500

    Function last version
    Final version for web scraping

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # runners-peru
+
+This repository collect and analyze data from running races in Peru.
+
+# Files structure
+
+```markdown
+├── Data                        -> File to download the data  in CSV format.
+
+├── Scripts
+  ├── '1 Web Scraping.py'       -> Program for web scrapping. Return a csv file with enconded utf-8 and a data frame.
+```
+
+# Steps to replicate
+
+```markdown
+├──   '1 Web Scraping.py'  
+      ├── If you not installed selenium write this command in your command prompt or terminal `pip install selenium`.
+      ├── In this project I used Google Chrome then you need the last version of Chromedriver.  Download the last version [here](https://chromedriver.chromium.org/downloads). Unzip the file and save in a path that you remember.     
+      ├── Run the function `webscraping_running_peru`. I put examples to web scrapping marathons but you can modify the code for web scrapping other running race just replace the number 11 with 10 in the lines 61, 66, 71, 76, 81, 86, 91, 96 and 101. Also you can modify the implicit wait according of your internet speed.
+
+  ```
+# Requirements
+
+- Google Chrome
+- The last version of Chromedriver
+- Python 3
--- Scripts/1 Web Scraping.py
+++ Scripts/1 Web Scraping.py
@@ -17,7 +17,7 @@ os.chdir(working_directory)
 def webscraping_running_peru(output_name,url,path_chromedriver):
     '''
     
-    Function for webscraping the results of running race in the page 
+    Function for webs craping the results of running race in the page 
     https://pacifictiming.com/resultados/
 
     Parameters
@@ -35,13 +35,16 @@ def webscraping_running_peru(output_name,url,path_chromedriver):
         
     Returns
     -------
-    A CSV with the data of a running race.
+    A CSV with the data of a running race and encoded with utf-8.
+    A data frame with the results of running race. 
+    Example: 
+        adidas_run = webscraping_running_peru(output_name,url,path_chromedriver)
 
     '''
 
     # Set scraping
     web = url # URl with the race running results
-    path = path_chromedriver #introduce your file's path inside '...'
+    path = path_chromedriver #introduce your file's path
     driver = webdriver.Chrome(path)
     driver.get(web)
     
@@ -51,52 +54,52 @@ def webscraping_running_peru(output_name,url,path_chromedriver):
     
     time.sleep(180) #add implicit wait, if necessary
     rows = driver.find_elements_by_class_name('result_cell')
-    for row in rows:
-        print(row.text)
+    # for row in rows:
+    #     print(row.text)
         
     number_runner = []  
     for i in range(0,len(rows),11):
-        print(rows[i].text)
+        # print(rows[i].text)
         number_runner.append(rows[i].text)
         
     last_name = [] 
     for i in range(1,len(rows),11):
-        print(rows[i].text)
+        # print(rows[i].text)
         last_name.append(rows[i].text)
     
     name = [] 
     for i in range(2,len(rows),11):
-        print(rows[i].text)
+        # print(rows[i].text)
         name.append(rows[i].text)
     
     gender = []
     for i in range(3,len(rows),11):
-        print(rows[i].text)
+        # print(rows[i].text)
         gender.append(rows[i].text)
     
     age = []
     for i in range(4,len(rows),11):
-        print(rows[i].text)
+        # print(rows[i].text)
         age.append(rows[i].text)
     
     category = []
     for i in range(5,len(rows),11):
-        print(rows[i].text)
+        # print(rows[i].text)
         category.append(rows[i].text)
         
     official_time = []
     for i in range(6,len(rows),11):
-        print(rows[i].text)
+        # print(rows[i].text)
         official_time.append(rows[i].text)
         
     chip_time = []
     for i in range(7,len(rows),11):
-        print(rows[i].text)
+        # print(rows[i].text)
         chip_time.append(rows[i].text)
            
     ranking = []
     for i in range(8,len(rows),11):
-        print(rows[i].text)
+        # print(rows[i].text)
         ranking.append(rows[i].text)
     
     dic = {'number_runner': number_runner,
@@ -110,19 +113,18 @@ def webscraping_running_peru(output_name,url,path_chromedriver):
            'ranking':ranking ,
            } 
     
-    output_name = pd.DataFrame(dic)  
-    output_name.to_csv('output_name.csv')
+    df = pd.DataFrame(dic)
+    df.to_csv(f'{output_name}.csv', sep = ',', index=False, encoding = 'utf-8')
+    return running_race 
 
 
-#links
-2017
-https://pacifictiming.com/result-wizard/?prefix=170521_lima42k
+# Example: Collect data from Marathon in Lima 2017-2019
+marathons = {'lima42k_2017':'https://pacifictiming.com/result-wizard/?prefix=170521_lima42k',
+             'lima42k_2018':'https://pacifictiming.com/result-wizard/?prefix=170521_lima42k',
+             'lima42k_2019':'https://pacifictiming.com/result-wizard/?prefix=190519_lima42k'
+             }
 
-2018
-https://pacifictiming.com/result-wizard/?prefix=180520_lima42k
-
-2019
-https://pacifictiming.com/result-wizard/?prefix=190519_lima42k
-
-
-https://pacifictiming.com/result-wizard/?prefix=170219_candelaria
+dfs = {}
+for running_race, link in marathons.items():
+    print(running_race, '->', link)
+    dfs[f'{running_race}'] = webscraping_running_peru(running_race,link,'D://Temp//chromedriver_win32//chromedriver.exe')
